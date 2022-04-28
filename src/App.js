0f2afdbf58589a31104ec9ab6660925a587ca689
Igor Gramkow <gramkowigor@gmail.com>
2022-04-28 14:42:17 -0300

    Primeiro Commit - Adicionando Header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Global from "./styles/global";
+import React from "react"
+import Header from "./components/header";
+function App() {
+  return (
+    <React.Fragment>
+      <Global /> 
+      <Header/>
+    </React.Fragment>
+    
+  );
+}
+
+export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function header() {
+  return (
+    <Container>
+      <h1>Gramkowfy</h1>
+    </Container>
+  );
+}
+
+export default header;
